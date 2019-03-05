551578be5830b7c16d1cc860d6489aedde434b11
filesmuggler <krzste09@outlook.com>
2019-03-05 22:23:46 +0100

    change

--- app/src/main/java/com/example/tarrc/MainActivity.java
+++ app/src/main/java/com/example/tarrc/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.tarrc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
+    CheckBox enable_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
