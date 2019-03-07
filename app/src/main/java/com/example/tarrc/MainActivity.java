8cc4f4a388acc217f49938025133fa54feb8f73a
filesmuggler <krzste09@outlook.com>
2019-03-07 23:54:12 +0100

    fixed toast

--- app/src/main/java/com/example/tarrc/MainActivity.java
+++ app/src/main/java/com/example/tarrc/MainActivity.java
@@ -16,10 +16,12 @@ package com.example.tarrc;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -37,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
     /* Bluetooth components */
     private BluetoothAdapter bluetoothAdapter;
     private Set<BluetoothDevice> pairedDevices;
+
+    static final int BLUETOOTH_REQUEST_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,8 +72,37 @@ public class MainActivity extends AppCompatActivity {
             enableButton.setChecked(true);
         }
 
+        enableButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(!isChecked){
+                    bluetoothAdapter.disable();
+                    Toast.makeText(MainActivity.this, "Turned off", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intentOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(intentOn,BLUETOOTH_REQUEST_CODE);
+
+                }
+
+            }
+        });
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    // Check which request we're responding to
+        if (requestCode == BLUETOOTH_REQUEST_CODE) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(MainActivity.this, "Turned on", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Operation aborted", Toast.LENGTH_SHORT).show();
+                enableButton.setChecked(false);
+            }
+
+        }
     }
 
     private String getLocalBluetoothName() {
