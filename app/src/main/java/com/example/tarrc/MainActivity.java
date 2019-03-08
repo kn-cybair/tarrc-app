b4484fe1fdcd35aca54dccd0306dc62b39178d64
filesmuggler <krzste09@outlook.com>
2019-03-08 12:02:58 +0100

    fixed Toast, added list of paired devices

--- app/src/main/java/com/example/tarrc/MainActivity.java
+++ app/src/main/java/com/example/tarrc/MainActivity.java
@@ -16,14 +16,19 @@ package com.example.tarrc;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
     /* Bluetooth components */
     private BluetoothAdapter bluetoothAdapter;
     private Set<BluetoothDevice> pairedDevices;
+
+    static final int BLUETOOTH_REQUEST_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,8 +75,71 @@ public class MainActivity extends AppCompatActivity {
             enableButton.setChecked(true);
         }
 
+        enableButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(!isChecked){
+                    bluetoothAdapter.disable();
+                    Toast.makeText(MainActivity.this, "Turned off", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intentOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(intentOn,BLUETOOTH_REQUEST_CODE);
+
+                }
+
+            }
+        });
+
+        visibleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                    startActivityForResult(getVisible,0);
+                    Toast.makeText(MainActivity.this, "Visible for 2 min", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                list();
+            }
+        });
 
 
+    }
+
+    private void list() {
+        pairedDevices = bluetoothAdapter.getBondedDevices();
+
+        ArrayList list = new ArrayList();
+        for (BluetoothDevice bt : pairedDevices){
+            list.add(bt.getName());
+        }
+
+        Toast.makeText(this, "Showing Devices", Toast.LENGTH_SHORT).show();
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        devicesList.setAdapter(adapter);
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == BLUETOOTH_REQUEST_CODE) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(MainActivity.this, "Turned on", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Operation aborted", Toast.LENGTH_SHORT).show();
+                enableButton.setChecked(false);
+            }
+
+        }
     }
 
     private String getLocalBluetoothName() {
